92f6480a32aca97b22b7ac273d55211475e4f135
Pankaj Kumar <jha.pankaj55@gmail.com>
2022-02-02 17:41:13 +0800

    #Tweet Cell

---
+++ twitterclone/View/SearchScreen.swift
@@ -0,0 +1,20 @@
+//
+//  SearchScreen.swift
+//  TwitterClone
+//
+//  Created by Pankaj Kumar Jha on 2/2/22.
+//
+
+import SwiftUI
+
+struct SearchScreen: View {
+    var body: some View {
+        Text("Hello Search").navigationTitle("Search")
+    }
+}
+
+struct SearchScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchScreen()
+    }
+}
