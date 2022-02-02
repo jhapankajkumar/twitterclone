15b78ae14ec5dd406b3d7a3cef08806da3ab484d
Pankaj Kumar <jha.pankaj55@gmail.com>
2022-02-02 19:11:40 +0800

    Search View (Static)

---
+++ twitterclone/View/Search/SearchView.swift
@@ -0,0 +1,29 @@
+//
+//  SearchView.swift
+//  TwitterClone
+//
+//  Created by Pankaj Kumar Jha on 2/2/22.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    @State var text: String = ""
+    var body: some View {
+        ScrollView {
+            PKSearchBar(text: $text)
+            VStack (alignment: .leading, spacing: 0) {
+                ForEach(0..<10) { _ in
+                    HStack { Spacer() }
+                    PKSearchCell()
+                }
+            }.padding(.leading)
+        }
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
