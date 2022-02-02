9ee659fc51d35a46e456697fe8cf7b05a5c1f72d
Pankaj Kumar <jha.pankaj55@gmail.com>
2022-02-02 14:17:01 +0800

    #Initial Project Commit

---
+++ twitterclone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Pankaj Kumar Jha on 31/1/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ twitterclone/View/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  TwitterClone
+//
+//  Created by Pankaj Kumar Jha on 31/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            TabView {
+                HomeScreen(title: "Home")
+                    .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                    }
+                
+                SearchScreen()
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                        Text("Search")
+                    }
+                
+                MessageScreen()
+                    .tabItem {
+                        Image(systemName: "envelope")
+                        Text("Messages")
+                    }
+            }.navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
